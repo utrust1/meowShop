05745eb205dc28b9dc9a73076b868adff7ac17ca
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-08 17:39:05 +0300

    test

--- backend/db/models/cart.js
+++ backend/db/models/cart.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cartModel = new mongoose.Schema({
-
     purchase: [{ type: mongoose.Schema.Types.ObjectId, ref: "product" }],
     Date: { type: Date, required: true },
     role: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
