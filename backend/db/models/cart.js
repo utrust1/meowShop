d9355a2452167cd3512e9cd190be95982dcb3b00
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-08 15:43:06 +0300

    create new cart schema

---
+++ backend/db/models/cart.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cartModel = new mongoose.Schema({
+
+    purchase: [{ type: mongoose.Schema.Types.ObjectId, ref: "product" }],
+    Date: { type: Date, required: true },
+    role: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+module.exports = mongoose.model("Cart", cartModel);
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
 const usersModel = new mongoose.Schema({
-  firstName: { type: String , required: true },
-  lastName: { type: String , required: true  },
-  age: { type: Number , required: true  },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 });
-
 module.exports = mongoose.model("User", usersModel);
-
-
-
-
-
