8f7e488db81e2157781ac3e49a17323369548e01
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-08 16:29:10 +0300

    ccccccccccc

---
+++ backend/db/models/category.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const categoryModel = new mongoose.Schema({
+    title: { type:String , required:true },
+    description: { type:String , required:true  },
+   
+  });
+  
+  module.exports = mongoose.model("Category", categoryModel);
+  
