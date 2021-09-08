c83948d802f9b8c53f8237faa7e1c9c99cc04a26
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-08 19:32:08 +0300

    create schema for purchas

--- backend/db/models/product.js
+++ backend/db/models/product.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
-
-
 const productModel = new mongoose.Schema({
-  title: { type: String , required: true },
-  description: { type: String , required: true  },
-  newprice: { type: Number , required: true  },
-  oldPrice: { type: Number , required: true  },
-  quantity: { type: Number, required: true },
-  category:{ type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true }
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    newprice: { type: Number, required: true },
+    oldPrice: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true }
 });
 
 module.exports = mongoose.model("Product", productModel);
