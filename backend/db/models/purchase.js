436b84f3dddfcb8164f997df00e590a57ffccee5
hushki94 <54278954+hushki94@users.noreply.github.com>
2021-09-08 20:42:26 +0400

    Update purchase.js

--- backend/db/models/purchase.js
+++ backend/db/models/purchase.js
@@ -5,7 +5,7 @@ const purchaseModel = new mongoose.Schema({
     address: { type: String, required: true },
     city: { type: String, required: true },
     phoneNumber: { type: Number, required: true },
-    country: { type: String, required: true, unique: true },
+    country: { type: String, required: true },
 });
 
-module.exports = mongoose.model("purchase", purchaseModel);
+module.exports = mongoose.model("Purchase", purchaseModel);
