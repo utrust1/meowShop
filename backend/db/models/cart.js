4047a0e3e7f6fd03889fcb58099dc71779d0c150
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-08 18:03:41 +0300

    create cart schema

--- backend/db/models/cart.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cartModel = new mongoose.Schema({
-    purchase: [{ type: mongoose.Schema.Types.ObjectId, ref: "product", required: true }],
-    Date: { type: Date, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-});
-module.exports = mongoose.model("Cart", cartModel);
