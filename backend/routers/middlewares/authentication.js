8b8297e069976b363028728c86444eda2d158dab
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-09 15:33:09 +0300

    cheack out the verify token

--- backend/routers/middlewares/authentication.js
+++ backend/routers/middlewares/authentication.js
@@ -1,6 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 const authentication  = (req,res,next)=>{
+try{
+    if(!req.headers.authorization){
+        return res.status(403).json({
+            success: false,  
+            message: `Forbidden`, 
+        })
+    }
 
+
+
+
+
+
+}
 }
 
