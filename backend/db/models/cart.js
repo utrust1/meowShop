a0044a1a5bfb1a0ec16fd664e0d06629362a82e7
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-09 22:20:48 +0300

    get default Date

--- backend/db/models/cart.js
+++ backend/db/models/cart.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const cartModel = new mongoose.Schema({
     purchase: [{ type: mongoose.Schema.Types.ObjectId, ref: "product", required: true }],
-    Date: { type: Date, required: true },
+    Date: { type: Date, default: Date.now },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
 module.exports = mongoose.model("Cart", cartModel);
