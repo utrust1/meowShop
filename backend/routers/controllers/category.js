f92d043ed0e58c061e18e524e676c622d42bd6e2
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-09 19:37:16 +0300

    update category using id

--- backend/routers/controllers/category.js
+++ backend/routers/controllers/category.js
@@ -43,10 +43,33 @@ const deleteCategoryById = (req, res) => {
         }
         res.json(filedDeleted);
     })
+}
 
+
+const updateCategoryById = (req, res) => {
+    id = req.params.id;
+    const { title, description } = req.body;
+    categoryModel.findOneAndUpdate({ _id: id }, { title, description }).then((result) => {
+        const successUpdated = {
+            success: true,
+            message: `Success updated`,
+            output: result
+        }
+        res.status(200);
+        res.json(successUpdated);
+
+    }).catch((err) => {
+        const filedUpdated = {
+            success: false,
+            message: `not found id ==> ${id} for this category`
+        }
+        res.status(500);
+        res.json(filedUpdated);
+    })
 }
 
 module.exports = {
     createNewCategory,
-    deleteCategoryById
+    deleteCategoryById,
+    updateCategoryById
 };
