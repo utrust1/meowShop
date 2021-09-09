23350380ee6ce344b442ec6c7f273ee12c91ec5d
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-10 00:18:40 +0300

    creating the navigation par

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Navigation from './components/navigation';
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div className="App">
+		<Navigation></Navigation>
+
+
+	</div>;
 };
 
 export default App;
