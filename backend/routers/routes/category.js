5244b04236dbc764aef638e790330b55cea05971
hushki94 <omarhushki94@gmail.com>
2021-09-10 00:11:59 +0400

    make category routers

--- backend/routers/routes/category.js
+++ backend/routers/routes/category.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { createNewCategory } = require("../controllers/category")
+const { createNewCategory,deleteCategoryById,
+    updateCategoryById } = require("../controllers/category")
 const cartegoryRouter = express.Router();
+
 cartegoryRouter.post("/", createNewCategory);
+cartegoryRouter.delete("/:id", deleteCategoryById);
+cartegoryRouter.put("/:id", updateCategoryById);
+
 
 
 
