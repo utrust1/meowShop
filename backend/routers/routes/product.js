8605390a9e34371611e446b45c1af571ffca7a91
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-09 20:26:08 +0300

    connect the path with  controllers function

--- backend/routers/routes/product.js
+++ backend/routers/routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createNewProduct, getAllProduct , getProductById, getProductByPrice, updateProductById, deleteProductById, getProductByCategory } = require("../controllers/product")
 const productRouter = express.Router();
 productRouter.post("/", createNewProduct);
-productRouter.get("/", )
+productRouter.get("/", getAllProduct )
 productRouter.get("/byprice", getProductByPrice)
 productRouter.get("/:id", getProductById)
 productRouter.put("/:id", updateProductById)
