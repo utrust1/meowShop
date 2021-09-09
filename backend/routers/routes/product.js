cee431b54c1989d545fe4229813762b847963bb2
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-09 19:32:08 +0300

    create get product router by category

--- backend/routers/routes/product.js
+++ backend/routers/routes/product.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { createNewProduct, getProductById, getProductByPrice , updateProductById ,deleteProductById } = require("../controllers/product")
+const { createNewProduct, getProductById, getProductByPrice , updateProductById ,deleteProductById ,getProductByCategory} = require("../controllers/product")
 const productRouter = express.Router();
 productRouter.post("/", createNewProduct);
 productRouter.get("/byprice", getProductByPrice)
 productRouter.get("/:id", getProductById)
 productRouter.put("/:id", updateProductById )
-productRouter.delete(":/id", deleteProductById )
+productRouter.delete("/:id", deleteProductById )
+productRouter.get("/:category",getProductByCategory )
+
+
 
 module.exports = productRouter;
