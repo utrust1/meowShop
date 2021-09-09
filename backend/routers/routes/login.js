1887c88cda389c0e7a335b3616e3de138e32dd49
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-09 15:51:52 +0300

    make the loginRouter in the auth folder

--- backend/routers/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { login } = require("../routes/auth/login");
-
-const loginRouter = express.Router();
-
-loginRouter.post("/", login);
-
-module.exports = loginRouter;
