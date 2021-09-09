1022f69a69d1f90cc0260ea6ff1c2d3af5cea050
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-09 16:37:04 +0300

    finish creating the purchase router

--- backend/routers/routes/purchase.js
+++ backend/routers/routes/purchase.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {createNewPurchase} = require("./../controllers/purchase")
 const purchaseRouter = express.Router();
 
-
+purchaseRouter.post("/", createNewPurchase)
 
 
 module.exports = purchaseRouter;
