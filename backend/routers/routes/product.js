b6578388fa23132d42af4b8009b0939edb331a39
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-09 20:24:06 +0300

    make the api

--- backend/routers/routes/product.js
+++ backend/routers/routes/product.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createNewProduct, getProductById, getProductByPrice, updateProductById, deleteProductById, getProductByCategory } = require("../controllers/product")
 const productRouter = express.Router();
 productRouter.post("/", createNewProduct);
+productRouter.get("", )
 productRouter.get("/byprice", getProductByPrice)
 productRouter.get("/:id", getProductById)
 productRouter.put("/:id", updateProductById)
