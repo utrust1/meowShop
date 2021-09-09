e9dd3a6483e54be1bc5e6822c9510f7dd112432e
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-10 01:25:54 +0300

    dsdsds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 const App = () => {
-    return <div className = "App" > App Component < /div>;
+    return <div className = "App" > App Component </div>;
 };
 export default App;
