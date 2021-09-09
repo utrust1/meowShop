ee58d3514b55c14904e0c4c5de74c6f14f8a429b
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-10 00:32:32 +0300

    modfyfile

--- backend/routers/routes/product.js
+++ backend/routers/routes/product.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const { createNewProduct, getAllProduct , getProductById, getProductByPrice, updateProductById, deleteProductById, getProductByCategory } = require("../controllers/product")
+const { createNewProduct, getAllProduct, getProductById, getProductByPrice, updateProductById, deleteProductById, getProductByCategory } = require("../controllers/product")
 const productRouter = express.Router();
 productRouter.post("/", createNewProduct);
-productRouter.get("/", getAllProduct )
+productRouter.get("/", getAllProduct)
 productRouter.get("/byprice", getProductByPrice)
 productRouter.get("/:id", getProductById)
 productRouter.put("/:id", updateProductById)
 productRouter.delete("/:id", deleteProductById)
-productRouter.get("/:category", getProductByCategory)
-
-
+productRouter.get("/category/:category", getProductByCategory)
 
 module.exports = productRouter;
