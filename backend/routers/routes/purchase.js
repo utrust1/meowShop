49700b0cb1912203240da4dd75a1f97ab5856196
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-09 16:35:48 +0300

    export the purchase router

--- backend/routers/routes/purchase.js
+++ backend/routers/routes/purchase.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const {createNewPurchase} = require("./../controllers/purchase")
 const purchaseRouter = express.Router();
+
+
+
+
+module.exports = purchaseRouter;
