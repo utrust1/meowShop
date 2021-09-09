f937d47ef54fd41f6a22fe13214a6cb3abba374d
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-09 15:37:13 +0300

    export the loginRouter

--- backend/routers/routes/login.js
+++ backend/routers/routes/login.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { login } = require("../routes/auth/login")
+const { login } = require("../routes/auth/login");
 
 const loginRouter = express.Router();
 
 loginRouter.post("/", login);
 
+module.exports = loginRouter;
