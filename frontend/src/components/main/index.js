459111b3d56b592f7532ba093655f57fe77709b3
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-10 20:41:37 +0300

    get name of the category inside main page

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,24 +1,30 @@
-import React , {useState ,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './main.css';
 
 const Main = () => {
 
-	useEffect(()=>{
-		
-	}, []);
+    useEffect(() => {
+		axios.get(`http://localhost:5000/category`).then((res) => {
+			setGetCategory(res.data.result);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, []);
+
+    const [getCategory, setGetCategory] = useState();
 
 
-	const getallCategory = () => {
-		axios.get(`'http://localhost:5000/articles'`);
-	}
 
 
-
-
-
-	return <div className="App">
-	</div>;
+    return <div className = "App" >
+		{getCategory&&
+		getCategory.map((cate)=>{
+			return (
+				<h1>{cate.title}</h1>
+			)
+		})}
+		</div>;
 };
 
 export default Main;
