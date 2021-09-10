6fa9106d55042e217f389f8d8cbbceb75763981b
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-10 17:28:35 +0300

    create simple header with background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Navigation from './components/navigation';
+import Header from './components/header';
 const App = () => {
-    return <div className = "App" > App Component </div>;
+    return <div className = "App">
+    <Navigation/>
+    <Header/>
+     </div>;
 };
 export default App;
--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './header.css';
 
 const Header = () => {
-	return <div className="App">Header component</div>;
+	return <div className="App">
+	   <header>
+        <div class='overlay'>
+            <div class='details-header'>
+                <h1>stone washed</h1>
+                <p>Faded in time for a timeless look</p>
+            </div>
+            <div class='icon-button'>
+			Hello
+            </div>
+        </div>
+    </header>
+	
+	</div>;
 };
 
 export default Header;
