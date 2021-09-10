707849501a7ed57e2b10f017c03888ec63240891
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-10 19:42:29 +0300

    create router for get all category

--- backend/routers/routes/category.js
+++ backend/routers/routes/category.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const { createNewCategory,deleteCategoryById,
-    updateCategoryById } = require("../controllers/category")
+const {
+    createNewCategory,
+    deleteCategoryById,
+    updateCategoryById,
+    getallCategory
+} = require("../controllers/category")
 const cartegoryRouter = express.Router();
 
 cartegoryRouter.post("/", createNewCategory);
 cartegoryRouter.delete("/:id", deleteCategoryById);
 cartegoryRouter.put("/:id", updateCategoryById);
-
+cartegoryRouter.get("/", getallCategory);
 
 
 
