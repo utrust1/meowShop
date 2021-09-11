27d30bf9550d0576a111b680a6dd967310b73515
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-11 15:30:07 +0300

    add events on the inputs and button

--- frontend/src/components/auth/signUp/index.js
+++ frontend/src/components/auth/signUp/index.js
@@ -18,13 +18,15 @@ const Register =()=> {
 
 
      return (
+         
         <div   className = "Register">
- <input></input>
- <input></input>
- <input></input>
- <input></input>
- <input></input>
- <button></button>
+
+ <input placeholder="firstName" onChange={(e)=>{setFirstName(e.target.value)}}></input>
+ <input placeholder="lastName" onChange={(e)=>{setlastName(e.target.value)}}></input>
+ <input placeholder="age" onChange={(e)=>{setAge(e.target.value)}}></input>
+ <input placeholder="email" onChange={(e)=>{setEmail(e.target.value)}}></input>
+ <input placeholder="password" onChange={(e)=>{setPassword(e.target.value)}}></input>
+ <button onClick={()=>{}}> Register </button>
 
 
             
