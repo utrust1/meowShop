45ae0f9c274c98a40e145d33a7d1656e0734ba86
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-12 02:39:11 +0300

    change img for category section

--- frontend/src/components/product/index.js
+++ frontend/src/components/product/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
+import './product.css'
 const GetAllProduct=(props)=> {
     const [getproduct , setGetproduct] = useState()
     const {id} = useParams()
@@ -12,13 +13,16 @@ const GetAllProduct=(props)=> {
         })
     }, []);
     return (
-        <div className='showPorduct'>
+        <div className='showProduct'>
             {getproduct&&
            getproduct.map((product) => {
                return(
                    <div class='ProductMain'>
                        <div>
                            <img src={product.img}/>
+                       </div>
+                       <div className='contactProduct'>
+                           <h1>{product.title}</h1>
                        </div>
                        
                    </div>
