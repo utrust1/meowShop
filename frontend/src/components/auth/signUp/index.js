5310bed725b86bf48439b9c918ac6ece10b6e497
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-11 15:24:12 +0300

    const the variabls for the user imformation

--- frontend/src/components/auth/signUp/index.js
+++ frontend/src/components/auth/signUp/index.js
@@ -1,3 +1,33 @@
 import "./signUp.css"
 import React from "react"
 import { useState ,  useEffect} from "react"
+
+
+
+const Register =()=> {
+   
+   const [firstName , setFirstName ] = useState("")
+   const [lastName , setlastName] = useState("")
+   const [age , setAge] = useState("")
+   const [email , setEmail] = useState("")
+   const [password , setPassword] =  useState("")
+   
+   
+
+
+
+   
+     return (
+
+
+
+        <div>
+
+
+
+            
+        </div>
+    )
+}
+
+export default Register
