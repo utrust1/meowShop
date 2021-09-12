975dcdd0bae667603fc10a8388a6d19a47e64053
hushki94 <omarhushki94@gmail.com>
2021-09-12 20:53:52 +0400

    capital letter update

--- frontend/src/components/auth/login/Login.js
+++ frontend/src/components/auth/login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from "react";
 import axios from "axios";
-import "./Login.css";
+import "./login.css";
 import { useHistory } from 'react-router';
 
 
