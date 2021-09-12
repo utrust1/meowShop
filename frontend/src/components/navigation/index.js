9c6c0c6a11b1c9d73f12de00d4d233f2ab739f23
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-12 03:44:29 +0300

    make default page named LAMA

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -7,14 +7,13 @@ import { IconName ,  FaUserTie , FaShoppingCart , FaSearch} from "react-icons/fa
 const Navigation = () => {
 	return <div className="container">
 	<div className='Navigation'> 
-		<p className='logoName'>LA<span>MA.</span></p>
+		<Link to='/Home'><p className='logoName'>LA<span>MA.</span></p> </Link>
 	     
 		<div className='Searchbar'>
 			<input type='text' placeholder='what do you want ' className= "SearchBarInput"></input> 
 					<FaSearch className="searchIcon"/> 
 		</div>
 		<div className="navbar">
-		<Link to="/Home" className='navLogin'>Home</Link>
 		<Link to="/login" className='navLogin'>Login</Link>
 		<Link to="/Register" className='navRegister'>Register</Link>
 		<Link to="/cart" className='navcart'><FaShoppingCart/></Link></div>
