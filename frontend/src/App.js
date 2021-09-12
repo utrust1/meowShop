7aef2ea4015301ce39341192e156206eb9dd2283
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-12 18:25:18 +0300

    modify token and undifined router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import {useState , createContext} from "react"
-import './App.css';
-import Navigation from './components/navigation';
-import Header from './components/header';
-import Main from './components/main';
-import ProductCategory from './components/category/ProductCategory';
-import Register from './components/auth/Register/Register';
-import GetAllProduct from './components/product/index';
-import Login from './components/auth/Login/Login';
-import { Switch } from 'react-router';
+import React from "react";
+import { Route } from "react-router-dom";
+import { useState, createContext } from "react";
+import "./App.css";
+import Navigation from "./components/navigation";
+import Header from "./components/header";
+import Main from "./components/main";
+import ProductCategory from "./components/category/ProductCategory";
+import Register from "./components/auth/Register/Register";
+import GetAllProduct from "./components/product/index";
+import Login from "./components/auth/Login/Login";
+import { Switch } from "react-router";
 export const UserContext = createContext();
 
 const App = () => {
+  const [token, setToken] = useState("");
 
-const [token , setToken]  = useState("")
-
-    return <div className = "App">
-    <Navigation/>
-    <UserContext.Provider value={token}>
-
-    <Route  path="/Home" component={Main} />
-    <Route  path="/category/:title/:id" component={ProductCategory}/>
-    <Route  path="/Register" component={Register} />
-    <Route path="/product/:id" component={GetAllProduct}/>
-    <Route path="/login" render={() => <Login setToken={setToken} /> }/>
-    </UserContext.Provider>
-     </div>;
+  return (
+    <div className="App">
+      <Navigation token={token} />
+<Switch>
+      <Route exact path="/Home" component={Main} />
+      <Route exact path="/category/:title/:id" component={ProductCategory} />
+      <Route exact path="/Register" component={Register} />
+      <Route exact path="/product/:id" component={GetAllProduct} />
+      <Route exact path="/login" render={() => <Login setToken={setToken} />} />
+      <Route path= "*" component={()=>"404 NOT FOUND"} />
+</Switch>
+    </div>
+  );
 };
 export default App;
--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,46 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import UserContext from "./../../App";
 import "./navigation.css";
 import { IconName, FaUserTie, FaShoppingCart, FaSearch } from "react-icons/fa";
 
-const Navigation = () => {
+const Navigation = ({ token }) => {
+  return (
+    <div className="container">
+      <div className="Navigation">
+        <Link to="/Home">
+          <p className="logoName">
+            LA<span>MA.</span>
+          </p>{" "}
+        </Link>
 
-let token = useContext(UserContext);
-
-  return <div className="container">
-	<div className='Navigation'> 
-		<Link to='/Home'><p className='logoName'>LA<span>MA.</span></p> </Link>
-	     
-		<div className='Searchbar'>
-			<input type='text' placeholder='what do you want ' className= "SearchBarInput"></input> 
-					<FaSearch className="searchIcon"/> 
-		</div>
-		<div className="navbar">
-		{token ? (
-	<div>
-	  <Link to="/cart" className="navcart">
-		
-	  </Link> 
-	  <FaShoppingCart />
-	  <Link to="/logout" className="navRegister">
-		log out 
-	  </Link>
-	</div>
-  ) : (
-	<div>
-	  <Link to="/login" className="navLogin">
-		Login
-	  </Link>
-	  <Link to="/Register" className="navRegister">
-		Register
-	  </Link>
-	</div>
-  )}</div>
-		</div>
-		 </div>;
-
-
+        <div className="Searchbar">
+          <input
+            type="text"
+            placeholder="what do you want "
+            className="SearchBarInput"
+          ></input>
+          <FaSearch className="searchIcon" />
+        </div>
+        <div className="navbar">
+          {token ? (
+            <div>
+              <Link to="/cart" className="navcart"></Link>
+              <FaShoppingCart />
+              <Link to="/logout" className="navRegister">
+                log out
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Link to="/login" className="navLogin">
+                Login
+              </Link>
+              <Link to="/Register" className="navRegister">
+                Register
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Navigation;
