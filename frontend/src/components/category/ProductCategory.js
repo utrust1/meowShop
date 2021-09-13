8886b27b7edd77e57ef7874d68cfd20e1c651723
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-13 15:56:08 +0300

    view product in privite page

--- frontend/src/components/category/ProductCategory.js
+++ frontend/src/components/category/ProductCategory.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Route  , useParams} from 'react-router-dom';
+import { useHistory  } from "react-router";
 import axios from 'axios';
 import './dashboard.css';
 const ProductCategory=(props)=> {
     const [product , setProduct] = useState()
     const {id} = useParams()
+    const history = useHistory()
     /// tseting the params
 	useEffect(() => {
 		axios.get(`http://localhost:5000/product/cat/${id}`).then((res) => {
@@ -13,10 +15,14 @@ const ProductCategory=(props)=> {
             console.log(err);
         })
     }, []);
+
+
+    const getbyid = (id) => {
+        history.push(`/product/${id}`)
+    }
     return (
         <div className="MainSectionForViewProduct">
-Hello
-            <div className='Contect-Main-Section'> 
+            <div className='Contect-Main-Section'>
             {product && product.map((elm)=>{
                 return (
                 <div className='viewProduct'>
@@ -24,7 +30,7 @@ Hello
                    <img src={elm.img}/>
                     <h1>{elm.title}</h1>
                     <p>{elm.description}</p>
-                    
+                    <button onClick={()=>{getbyid(elm._id)}}>Show</button>
                    </div>
                 </div>)
             })}
