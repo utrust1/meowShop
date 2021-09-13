52101663e2a59f5a046ca9888f120bdbc4c8d130
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-13 20:17:05 +0300

    preparing the  footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -3,11 +3,22 @@ import React from 'react';
 
 const Footer = () => {
 
+return <div className="Footer"> 
+ 
+<div className="our infprmation">
+	<p>About us </p>
+	<p>Contact us </p>
+</div>
+
+<div  className="footer icons">
+	<a className="icon" href="#"> <i className="fa fa-facebook"></i></a>
+	<a className="icon" href="#"> <i className=""></i></a>
+</div>
 
 
 
 
-	return <div className="App">Footer ssssssss</div>;
+</div>;
 };
 
 export default Footer;
