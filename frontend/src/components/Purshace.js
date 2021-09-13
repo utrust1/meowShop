a21419f9282aa17f89759f08c6b9eaa9d50d9d92
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-13 17:32:55 +0300

    prepare the variables and the functions using useState

--- frontend/src/components/Purshace.js
+++ frontend/src/components/Purshace.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {useState} from "react"
 
-function Purshace() {
+
+
+
+const  Purshace =()=>{
+
+
+   const [address ,setAddress] = useState("")
+   const [city , setCity ] = useState("")
+   const [phoneNumber ,setPhoneNumber ] = useState("")
+   const [country ,setCountry] =  useState("")
+
+
+
+
     return (
         <div>
+
             
         </div>
     )
