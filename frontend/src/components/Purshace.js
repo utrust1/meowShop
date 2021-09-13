6113e16b95ec7a9bb8360900fe5638acee926e2d
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-13 17:39:28 +0300

    insert the inputs fileds

--- frontend/src/components/Purshace.js
+++ frontend/src/components/Purshace.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import {useState} from "react"
 
-
-
-
 const  Purshace =()=>{
-
-
-   const [address ,setAddress] = useState("")
+       
+  const [address ,setAddress] = useState("")
    const [city , setCity ] = useState("")
    const [phoneNumber ,setPhoneNumber ] = useState("")
    const [country ,setCountry] =  useState("")
@@ -15,8 +11,15 @@ const  Purshace =()=>{
 
 
 
-    return (
+return (
         <div>
+
+        <input type="text"   placeholder =" Your Country "></input>
+        <input type="text"   placeholder =" Your City "></input>
+        <input type="text"   placeholder =" Your Address "></input>
+        <input type="number" placeholder =" Your Phone number"></input>
+        <button onClick={()=>{}}>Check out</button>
+
 
             
         </div>
