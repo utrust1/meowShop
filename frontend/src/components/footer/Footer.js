b80b7498fba1be126b21cd900c41f35aef9b0317
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-09-13 21:53:38 +0300

    preparing the  footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -13,16 +13,16 @@ const Footer = () => {
 const history =useHistory() 
 
 	const aboutUsEvent = ()=>{
-
+        history.push("/AboutUs")
 	}
 	
 
 	const ourPolicyEvent = ()=>{
-
+		history.push("/ContactUs")
 	}
 
 	const contactUsEvent = ()=>{
-
+		history.push("/OurPolicy")
 
 	}
 	
@@ -34,7 +34,7 @@ return <div className="FooterPerant">
 <div className="footerChild">
 	<p onClick={()=>{aboutUsEvent()}}>About us </p>
 	<p onClick={()=>{ourPolicyEvent()}}>Our Policy</p>
-	<span onClick={()=>{contactUsEvent()}} ><FaEnvelope  className="emailIcon" />Contact us</span>
+	<span onClick={()=>{contactUsEvent()}} >Contact us <FaEnvelope  className="emailIcon" /></span>
 </div>
 
 
