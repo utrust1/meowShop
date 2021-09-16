a58fdf3fa4b2f9ef6479b74dcfde3c165b73d3e7
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-17 02:18:04 +0300

    edit style

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -8,26 +8,28 @@ import {tokenContext} from '../../App'
 
 
 const Cart =  () => {
+  let b 
+    let total  = 0; 
     let token = useContext(tokenContext)
     const [insideCart, setInsideCart] = useState([]);
-    const [price , setPrice] = useState(0)
+    const [price , setPrice] = useState([])
+    let carts = {}
     const history = useHistory()
-    
     useEffect(()=>{getAllCart()},[])
+    console.log(" caaaaa " ,carts);
     
     const getAllCart = () => {
+      console.log("memmmw");
       axios
       .get(`http://localhost:5000/cart`,{ 
         headers: { Authorization: `Bearer ${token}`}})
-        .then( (result) => {
-          console.log("res222" , result.data.products );
-          setInsideCart(result.data.products )
-          setPrice(result.data.products.purchase[0]._id)
+        .then( (result) => {         
+          setInsideCart(result.data.products)
+          setPrice(result.data.products)
         })
         .catch((err) => {
           console.log(err);
-          
-        });
+        });   
       }
       
       
@@ -51,24 +53,40 @@ const Cart =  () => {
     <div className="CartPerant">
       <h4>SHOPPING CART</h4>
       {insideCart&&insideCart.map((elem )=>{
+         total+= elem.purchase[0].newprice
+         if(carts[elem.purchase[0]._id]){
+          carts[elem.purchase[0]._id]+=1 
+            return  ( <div>  {carts[elem.purchase[0]._id]} </div>  )            
+          }else{
+                carts[elem.purchase[0]._id]=1         
+              }
           return( <>
           <div className="CartBox">
             <div>
             <img  src={elem.purchase[0].img }/>
             </div>
+    
+    
             <div className="CartDes">
             <h3>{elem.purchase[0].title}</h3>
-            <p className="cartPrice">{elem.purchase[0].newprice} JD</p></div>
-        
+            <p className="cartPrice">{elem.purchase[0].newprice} JD</p>
+            </div>
+    
+
+
             <div>
             <button className='btnCart'onClick={()=>{deleteCart(elem._id)}} >Delete</button>
             </div>
           </div>
           <hr></hr>
-            </>)       
+
+            </>)   
+
       })
     }
-    <div className="checkout-btn">{(insideCart && insideCart)?(<button onClick={checkout}> check out </button>):(<p>""</p>)}</div>
+    <div className="checkout-btn">
+      <p> total : {total}</p>
+      {(insideCart && insideCart)?(<button onClick={checkout}> check out </button>):(<p>""</p>)}</div>
     </div>
     </div>
   );
@@ -77,3 +95,4 @@ const Cart =  () => {
 
 
 export default Cart;
+
