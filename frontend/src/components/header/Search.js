a15d320ea74a7383b855ced5adb2b492fb6d9ae8
hushki94 <omarhushki94@gmail.com>
2021-09-17 18:43:16 +0400

    create a file for search css

--- frontend/src/components/header/Search.js
+++ frontend/src/components/header/Search.js
@@ -1,19 +1,21 @@
-import React, { useState, useEffect , useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useHistory } from "react-router";
 import { searchContext } from "../../App";
 import axios from "axios";
 
+export const Search = () => {
+  let searchBar = useContext(searchContext);
 
-export const Search = () =>{
-    let searchBar = useContext(searchContext)
-    console.log("sosososooss",searchBar)
-    return <div>{searchBar.map((elm)=>{
-       console.log(elm.img)
+  return (
+    <div className="container">
+        <div cla></div>
+      {searchBar.map((elm) => {
         return (
-            
-        <div>{elm.title}  <img src={elm.img}/></div> )
-      
-       
-       
-    })}</div>
-}
+          <div className="searchProduct">
+            {elm.title} <img src={elm.img} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
