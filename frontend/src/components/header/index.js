b4af4431ae7279d0bdf1ab3466af5db673aedc29
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-17 16:32:04 +0300

    slider css

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import SliderImg from './SliderImg';
 import './header.css';
 
 const Header = () => {
 	return <div className="container">
-	   <header>
-         <img src='https://images.ctfassets.net/e43mbx7oxv8s/dTeQVZbR72uiYkEAYKyUS/bd3825a93aace2c7b368a7a742b1000f/000122_ZV_SCSW_HeroImages_1900x1024_D1.jpg'/>
-    </header>
-    
+		 {SliderImg.map((slider , index) => {
+			 return(
+				 <img src=
+					{slider.image}>
+				 </img>
+			 )
+
+		 })}
+	
 	</div>;
 };
 
