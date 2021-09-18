8b233aceea0a4b3a7c20da47b2049dcd86338129
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-18 14:41:37 +0300

    save the token

--- frontend/src/components/auth/login/Login.js
+++ frontend/src/components/auth/login/Login.js
@@ -19,6 +19,7 @@ const buttonEvent = ()=>{
 axios.post(`http://localhost:5000/login/`, {email,password}).then((result)=>{
     console.log(result);
     setToken(result.data.token)
+    localStorage.setItem("token" , result.data.token )
     setCheckLogout(true)
     console.log(result.data.token);
 }).catch((error)=>{
